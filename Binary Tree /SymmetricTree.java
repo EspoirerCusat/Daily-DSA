9c6f357b8aa1c2a169fe302318c5f5d1ff1b278a
Ravindra kumar <115468422+EspoirerCusat@users.noreply.github.com>
2025-10-07 00:07:15 +0530

    Create SymmetricTree.java

---
+++ Binary Tree /SymmetricTree.java
@@ -0,0 +1,16 @@
+
+//https://leetcode.com/problems/symmetric-tree/
+
+class Solution {
+    public boolean f(TreeNode node1, TreeNode node2){
+        if(node1 == null && node2 == null) return true;
+        if(node1 == null || node2 == null) return false;
+        if(node1.val != node2.val) return false;
+        return f(node1.left, node2.right) && f(node1.right, node2.left);
+    }
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+
+        return f(root.left, root.right);
+    }
+}
